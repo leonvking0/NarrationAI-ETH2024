ea4b4874af6f2352506fa7e85998eff6aa9c8733
Han Xu <leonvking0@users.noreply.github.com>
2024-07-12 22:34:53 +0200

    FE first commit
    FE first commit

---
+++ src/statics/Utils.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        background: {
+            paper: '#CCD3CA',
+            default: "#F5E8DD",
+        },
+        text: {
+            primary: '#173A5E',
+            secondary: '#46505A',
+        },
+        action: {
+            active: '#001E3C',
+        },
+    },
+});
